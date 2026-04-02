aab1e9d7384ad81316114eb0838cf10c3a205de4
Loudbie <papa280774@gmail.com>
2026-04-02 13:52:58 +0300

    handlers.go Added possibility to test. Also return error without string
    Some changes in routes.go for correct work

--- Form/handlers/handlers.go
+++ Form/handlers/handlers.go
@@ -1,39 +1,47 @@
 package handlers
 
 import (
-	"DemoFormTutor/Form/database"
 	"DemoFormTutor/Form/structs"
-	"strconv"
+	"database/sql"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 /*
-TODO Возможность протестировать
 TODO Переиспользование кода (разделение на части)
-TODO Возврат без строки, вместо неё нужна ошибка
 TODO Проверку на дубликат вводных данных
 
 Выполнено:
 Возврат записанных данных
+Возможность протестировать
+Возврат без строки, вместо неё ошибка
 */
+type DatabaseExecutor interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
 
-func SaveTutor(c *fiber.Ctx) error {
+type TutorHandler struct {
+	DB DatabaseExecutor
+}
+
+func (h *TutorHandler) SaveTutor(c *fiber.Ctx) error {
 	tutor := new(structs.Tutor)
 	if err := c.BodyParser(tutor); err != nil {
 		return c.Status(400).SendString("Неверный формат запрос")
 	}
-	_, err := database.Connect().Exec("INSERT INTO tutor (name, email, expworktime, expectation, needcourses, tutorbefore) VALUES ($1, $2, $3, $4, $5, $6)", tutor.Name, tutor.Email, tutor.ExpWorkTime, tutor.Expectation, tutor.NeedCourses, tutor.TutorBefore)
+	_, err := h.DB.Exec("INSERT INTO tutor (name, email, expworktime, expectation, needcourses, tutorbefore) VALUES ($1, $2, $3, $4, $5, $6)", tutor.Name, tutor.Email, tutor.ExpWorkTime, tutor.Expectation, tutor.NeedCourses, tutor.TutorBefore)
 	if err != nil {
 		return c.Status(500).SendString("Ошибка вставки данных в базу")
 	}
-	return c.Status(201).SendString(
-		"Репетитор успешно добавлен:" +
-			"\nИмя: " + tutor.Name +
-			"\nEmail:" + tutor.Email +
-			"\nОпыт работы: " + tutor.ExpWorkTime +
-			"\nОжидания от работы: " + tutor.Expectation +
-			"\nНеобходимость курсов: " + strconv.FormatBool(tutor.NeedCourses) +
-			"\nРаботал ли до этого: " + strconv.FormatBool(tutor.TutorBefore),
-	)
+	return c.Status(201).JSON(fiber.Map{
+		"message": "Репетитор успешно добавлен:",
+		"tutor": fiber.Map{
+			"Имя":                    tutor.Name,
+			"Почта":                  tutor.Email,
+			"Опыт работы":            tutor.ExpWorkTime,
+			"Ожидания от работы":     tutor.Expectation,
+			"Нужны ли курсы":         tutor.TutorBefore,
+			"Преподавал ли до этого": tutor.TutorBefore,
+		},
+	})
 }
--- Form/routes/routes.go
+++ Form/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"DemoFormTutor/Form/database"
 	"DemoFormTutor/Form/handlers"
 
 	"github.com/gofiber/fiber/v2"
@@ -8,10 +9,12 @@ import (
 
 func RegisterRoutes(app *fiber.App) {
 	api := app.Group("/tutor")
+	localDB := database.Connect()
 
 	api.Get("/", func(c *fiber.Ctx) error { //Test
 		return c.JSON(fiber.Map{})
 	})
 
-	api.Post("/save-tutor-data/:id", handlers.SaveTutor) //Add Tutor's info
+	handler := &handlers.TutorHandler{DB: localDB}
+	api.Post("/save-tutor-data/:id", handler.SaveTutor) //Add Tutor's info
 }
